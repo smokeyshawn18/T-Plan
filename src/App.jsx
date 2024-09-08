13a2dd826cb884a1e374fd7b2b95f7ca4c877b45
Shudar_shawn <shudarsanpoudel25@gmail.com>
2024-09-08 20:40:57 +0545

    Doneee

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import Home from "./components/Home.jsx";
 import Navbar from "./components/Navbar.jsx";
-import { HashRouter, Routes } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
 
 function App() {
   return (
     <>
       <HashRouter>
+        {/* Navbar is outside Routes since it's part of the layout */}
+        <Navbar />
+
+        {/* Routes should only contain Route components */}
         <Routes>
-          <Navbar />
-          <Home />
+          <Route path="/" element={<Home />} />
+          {/* Add more routes here as needed */}
         </Routes>
       </HashRouter>
     </>
